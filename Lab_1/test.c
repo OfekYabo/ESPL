d251d915301113faf0e724aefce7cfbc810713d5
OfekYabo <yaboofe@post.bgu.ac.il>
2024-11-19 16:22:03 +0200

    .

--- Lab_1/test.c
+++ Lab_1/test.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+int f(int a, int b)
+{
+    return a + b;
+}
+
 int main(void)
 {
-    printf("Hello, World!\n");
+    printf(sizeof(long));
+    int (*function)(int, int);
+    function = f;
+    printf("%d\n", function(1, 2));
     return 0;
 }
