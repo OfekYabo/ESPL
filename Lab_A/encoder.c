0eea611737f8807f9dbe4703e09e58598c7815e1
OfekYabo <yaboofe@post.bgu.ac.il>
2024-11-30 06:44:27 +0200

    part 2 finish

--- Lab_A/encoder.c
+++ Lab_A/encoder.c
@@ -7,8 +7,32 @@
 FILE* infile;
 FILE* outfile;
 bool debug_mode = 1;
+char* encoding_key = "0"; // Default encoding key
+int key_len = 0;
+int key_index = 0;
+
+int encode_add(int key_value, char c) {
+    return c + key_value;
+}
+
+int encode_subtract(int key_value, char c) {
+    return c - key_value;
+}
+
+int (*encode_mode)(int key_value, char c) = encode_add;
 
 char encode(char c) {
+    int key_value = encoding_key[key_index] - '0';
+
+    if (c >= 'A' && c <= 'Z') {
+        c = (encode_mode(key_value, c - 'A') + 26) % 26 + 'A';
+    } else if (c >= 'a' && c <= 'z') {
+        c = (encode_mode(key_value, c - 'a') + 26) % 26 + 'a';
+    } else if (c >= '0' && c <= '9') {
+        c = (encode_mode(key_value, c - '0') + 10) % 10 + '0';
+    }
+
+    key_index = (key_index + 1) % key_len; // Cycle through the key
     return c;
 }
 
@@ -18,6 +42,16 @@ void handle_arguments(int argc, char **argv) {
             debug_mode = 0;
         } else if (strcmp(argv[i], "+D") == 0){
             debug_mode = 1;
+        } else if (strncmp(argv[i], "+E", 2) == 0){
+            encoding_key = argv[i] + 2;
+            key_len = strlen(encoding_key);
+            key_index = 0;
+            encode_mode = encode_add;
+        } else if (strncmp(argv[i], "-E", 2) == 0){
+            encoding_key = argv[i] + 2;
+            key_len = strlen(encoding_key);
+            key_index = 0;
+            encode_mode = encode_subtract;
         }
     }
     if (debug_mode == 1) {
