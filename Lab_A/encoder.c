06df145f4fdcc4af5546d897f2d656673c26cd0b
OfekYabo <yaboofe@post.bgu.ac.il>
2024-11-30 08:01:00 +0200

    fixed before submite

--- Lab_A/encoder.c
+++ Lab_A/encoder.c
@@ -4,25 +4,27 @@
 #include <stdbool.h>
 
 //my code helped by copilot
+//part 1
 FILE* infile;
 FILE* outfile;
 bool debug_mode = 1;
+//part 2
 char* encoding_key = "0"; // Default encoding key
 int key_len = 0;
 int key_index = 0;
 
-int encode_add(int key_value, char c) {
+int encode_add(int key_value, char c) { //part 2
     return c + key_value;
 }
 
-int encode_subtract(int key_value, char c) {
+int encode_subtract(int key_value, char c) { //part 2
     return c - key_value;
 }
 
-int (*encode_mode)(int key_value, char c) = encode_add;
+int (*encode_mode)(int key_value, char c) = encode_add; //part 2
 
 //my code helped by copilot
-char encode(char c) {
+char encode(char c) { //part 2
     int key_value = encoding_key[key_index] - '0';
 
     if (c >= 'A' && c <= 'Z') {
@@ -40,27 +42,25 @@ char encode(char c) {
 //my code helped by copilot
 void handle_arguments(int argc, char **argv) {
     for (int i = 1; i < argc; i++){
-        if (strcmp(argv[i], "-D") == 0){
+        if (strcmp(argv[i], "-D") == 0){ //part 1
             debug_mode = 0;
-        } else if (strcmp(argv[i], "+D") == 0){
+        } else if (strcmp(argv[i], "+D") == 0){ //part 1
             debug_mode = 1;
-        } else if (strncmp(argv[i], "+E", 2) == 0){
+        } else if (strncmp(argv[i], "+E", 2) == 0){ //part 2
             encoding_key = argv[i] + 2;
-            key_len = strlen(encoding_key);
             key_index = 0;
             encode_mode = encode_add;
-        } else if (strncmp(argv[i], "-E", 2) == 0){
+        } else if (strncmp(argv[i], "-E", 2) == 0){ //part 2
             encoding_key = argv[i] + 2;
-            key_len = strlen(encoding_key);
             key_index = 0;
             encode_mode = encode_subtract;
-        } else if (strncmp(argv[i], "-i", 2) == 0) {
+        } else if (strncmp(argv[i], "-i", 2) == 0) { //part 3
             infile = fopen(argv[i] + 2, "r");
             if (infile == NULL) {
                 fprintf(stderr, "Error opening input file: %s\n", argv[i] + 2);
                 exit(1);
             }
-        } else if (strncmp(argv[i], "-o", 2) == 0) {
+        } else if (strncmp(argv[i], "-o", 2) == 0) { //part 3
             outfile = fopen(argv[i] + 2, "w");
             if (outfile == NULL) {
                 fprintf(stderr, "Error opening output file: %s\n", argv[i] + 2);
@@ -68,7 +68,7 @@ void handle_arguments(int argc, char **argv) {
             }
         }
     }
-    if (debug_mode == 1) {
+    if (debug_mode == 1) { //part 1
         for (int i = 0; i < argc; i++){
             fprintf(stderr, "Argument %d: %s\n", i, argv[i]);
         }
@@ -76,13 +76,13 @@ void handle_arguments(int argc, char **argv) {
 }
 
 //my code
-void init_files() {
+void init_files() { //part 1
     infile = stdin;
     outfile = stdout;
 }
 
 //my code helped by copilot
-void close_files() {
+void close_files() { //part 1
     if (infile != stdin) {
         fclose(infile);
     }
@@ -95,9 +95,16 @@ void close_files() {
 
 //my code helped by copilot
 int main(int argc, char **argv) {
+    //part 1
     init_files();
     handle_arguments(argc, argv);
 
+    //part 2
+    while (encoding_key[key_len]) {
+        key_len++;
+    }
+
+    //part 1
     int c;
     while ((c = fgetc(infile)) != EOF) {
         char encoded_char = encode((char)c);
