0c4d548d70a0c672e37767ddc384a2772acd7b0e
OfekYabo <yaboofe@post.bgu.ac.il>
2024-12-24 15:33:10 +0200

    finish task 1 before testing

--- Lab_3/task1/start.s
+++ Lab_3/task1/start.s
@@ -80,7 +80,7 @@ main: ; int main(int argc, char *argv[])
     call encode_input
 
     ; Exit the program
-    move eax, 0             ; return 0 (success)
+    mov eax, 0             ; return 0 (success)
     ret
 
 parse_args: ; void parse_args(int argc, char *argv[])
@@ -94,82 +94,114 @@ parse_args: ; void parse_args(int argc, char *argv[])
         cmp ecx, 0
         jmp return
 
-        push ecx                ; save argc
         push esi                ; save argv
+        push ecx                ; save argc
 
         push dword [esi]        ; push argv[i] as argument to print_arg
         call check_i_option     ; check for -i option
-        cmp eax, 0
+        cmp eax, 0              ; if -i option is found, skip -o check
         je continue
-        call check_o_option     ; check for -i option
+        call check_o_option     ; check for -o option
         
         continue:
-            add esp, 4              ; remove the argument from the stack
+            add esp, 4              ; remove the argument argv[i] from the stack
 
-            pop esi                 ; restore argv
             pop ecx                 ; restore argc
+            pop esi                 ; restore argv
 
-        ; Move to the next argument
-        dec ecx                 ; decrement argc
-        add esi, 4              ; esi += 4 (next argument)
-        jmp parse_args_loop     ; loop
-
-
-
-
-        check_i_option:
-            push ecx            ; save argc
-            push esi            ; save argv
-
-            push [esi]          ; push argv[i] as argument to strncmp
-            push IntputFile_String ; "-i"
-            push dword 2        ; length of "-i"
-            call strncmp
-            add esp, 12         ; clean arguments from the stack
-
-            pop esi             ; restore esi
-            pop ecx             ; restore ecx
-
-            cmp eax, 0
-            jne check_o_option
-            move esi, [esi +2]             ; skip -i
-            push esi
+            ; Move to the next argument
+            dec ecx                 ; decrement argc
+            add esi, 4              ; esi += 4 (next argument)
+            jmp parse_args_loop     ; loop
 
 
-        cmp byte [edi], '-'
-        jne parse_args_loop
-        cmp byte [edi + 1], 'i'
-        jne check_o_option
-        add edi, 2             ; skip -i
-        push edi
-        call open_input_file
-        jmp parse_args_loop
 
-check_o_option:
-    ; Check for -o option
-    cmp byte [edi + 1], 'o'
-    jne parse_args_loop
-    add edi, 2             ; skip -o
-    push edi
-    call open_output_file
-    jmp parse_args_loop
 
-parse_args_done:
+check_i_option: ; int check_i_option(char *arg)
+; Check if the argument start with the -i option.
+    
+    mov esi, [esp + 4]      ; get argv[i] argument
+    push esi                ; save argument argv[i]
+
+    push dword 2            ; length of "-i"
+    push IntputFile_String  ; "-i"
+    push esi                ; pass argument to strncmp
+    call strncmp
+    add esp, 12             ; clean arguments from the stack
+
+    pop esi                 ; restore argument argv[i]
+
+    cmp eax, 0
+    jne return
+
+    add esi, 2              ; skip the first 2 characters ("-i")
+
+    push esi                ; pass the file name to open_input_file
+    call open_input_file
+    add esp, 4              ; clean argument from the stack
+    mov eax, 0             ; return 0 (true)
     ret
 
-open_input_file:
-    ; Open the input file specified by -i{file}
-    pop ebx                ; get the file name
+check_o_option: ; int check_o_option(char *arg)
+; Check if the argument start with the -o option.
+    
+    mov esi, [esp + 4]      ; get argv[i] argument
+    push esi                ; save argument argv[i]
+
+    push dword 2            ; length of "-o"
+    push OutputFile_String  ; "-o"
+    push esi                ; pass argument to strncmp
+    call strncmp
+    add esp, 12             ; clean arguments from the stack
+
+    pop esi                 ; restore argument argv[i]
+
+    cmp eax, 0
+    jne return
+
+    add esi, 2              ; skip the first 2 characters ("-o")
+
+    push esi                ; pass the file name to open_output_file
+    call open_output_file
+    add esp, 4              ; clean argument from the stack
+    mov eax, 0             ; return 0 (true)
+    ret
+
+
+; cmp byte [edi], '-'
+; jne parse_args_loop
+; cmp byte [edi + 1], 'i'
+; jne check_o_option
+; add edi, 2             ; skip -i
+; push edi
+; call open_input_file
+; jmp parse_args_loop
+
+; check_o_option:
+;     ; Check for -o option
+;     cmp byte [edi + 1], 'o'
+;     jne parse_args_loop
+;     add edi, 2             ; skip -o
+;     push edi
+;     call open_output_file
+;     jmp parse_args_loop
+
+
+open_input_file: ; void open_input_file(char *filename)
+; Open the input file specified by -i{file}
+
     mov eax, sys_open      ; sys_open
+    mov ebx, [esp + 4]     ; get Input filename argument
     mov ecx, O_RDONLY      ; read-only mode
     int 0x80               ; call kernel
     mov [Infile], eax      ; store the file descriptor in Infile
     ret
 
-open_output_file:
-    ; Open the output file specified by -o{file}
-    pop ebx                ; get the file name
+open_output_file: ; void open_output_file(char *filename)
+; Open the output file specified by -o{file}
+
     mov eax, sys_open      ; sys_open
+    mov ebx, [esp + 4]     ; get Output filename argument
     mov ecx, O_WRONLY | O_CREAT | O_TRUNC ; write-only, create, truncate
     mov edx, 0666          ; file permissions
     int 0x80               ; call kernel
@@ -187,15 +219,15 @@ print_args: ; void print_args(int argc, char *argv[])
         cmp ecx, 0
         jmp return
         
-        push ecx                ; save argc
         push esi                ; save argv
+        push ecx                ; save argc
 
         push dword [esi]        ; push argv[i] as argument to print_arg
         call print_arg          ; print the argument
         add esp, 4              ; remove the argument from the stack
 
-        pop esi                 ; restore esi
         pop ecx                 ; restore ecx
+        pop esi                 ; restore esi
 
         ; Move to the next argument
         dec ecx                 ; decrement argc
@@ -214,7 +246,7 @@ print_arg: ; void print_arg(char *arg)
 
         ; Print the current character
         mov eax, sys_write      ; sys_write
-        mov ebx, stdout         ; stdout
+        mov ebx, Outfile        ; Outfile (default stdout)
         mov ecx, esi            ; pointer to the character
         mov edx, 1              ; length = 1
         int 0x80                ; call kernel
@@ -226,19 +258,19 @@ print_arg: ; void print_arg(char *arg)
     print_newline:
         ; Print a newline
         mov eax, sys_write      ; sys_write
-        mov ebx, stdout         ; stdout
-        lea ecx, [newline]      ; load pointer to newline label
+        mov ebx, Outfile        ; Outfile (default stdout)
+        mov ecx, newline        ; load pointer to newline label
         mov edx, 1              ; length = 1
         int 0x80                ; call kernel
 
         ret
 
 encode_input: ; void encode_input()
-; Read from stdin and encode the input to stdout.
+; Read from Inputfile (default stdin) and encode the input to Outputfile(default stdout).
 
     mov eax, sys_read       ; sys_read
     mov ebx, [Infile]       ; stdin by default
-    lea ecx, [buffer]
+    mov ecx, buffer         ; buffer to store the input  
     mov edx, 1              ; length = 1
     int 0x80                ; call kernel
 
@@ -250,12 +282,17 @@ encode_input: ; void encode_input()
     je return
 
     ; Encode the character
-    cmp byte [ebx], 'A'
+    cmp byte [esi], 'A'
     jb write_char
-    cmp byte [ebx], 'z'
+    cmp byte [esi], 'Z'
+    jbe increment_char
+    cmp byte [esi], 'a'
+    jb write_char
+    cmp byte [esi], 'z'
     ja write_char
 
-    inc byte [ebx]
+    increment_char:
+    inc byte [esi]        ; increment the character
 
     write_char:
         ; Write the encoded character to stdout
